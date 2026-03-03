94a2298f8047a07f698d3ebf53deae94836ba426
Christopher Owens <raddix1985@live.co.za>
2026-03-03 11:52:45 +0200

    Set sanity to use CDN

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
-  production: false,
+  production: true,
   sanity: {
     projectId: 'zr082jb6',
     dataset: 'production',
     apiVersion: '2025-01-01',
-    useCdn: false,
+    useCdn: true,
   },
   web3forms: {accessKey: '116013b9-9ce0-411b-b135-6550b16ccc51',
 },
