2a77e94b9b0a35025ee51b9cf242e4329f85267d
Christopher Owens <raddix1985@live.co.za>
2026-03-04 09:59:31 +0200

    Added provideClientHydration to reduce sanity calls

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideClientHydration } from '@angular/platform-browser';
 
 import { routes } from './app.routes';
 
@@ -12,8 +13,10 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withInMemoryScrolling({
       anchorScrolling: 'enabled',
+      
       scrollPositionRestoration: 'top', 
     })),
     provideHttpClient(withFetch()),
+    provideClientHydration(),
   ],
 };
