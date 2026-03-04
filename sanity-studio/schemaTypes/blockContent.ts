ac35d14ad46501b92fd820a53957b47180623fa1
Christopher Owens <raddix1985@live.co.za>
2026-03-04 16:48:45 +0200

    sanity schema for articles page

---
+++ sanity-studio/schemaTypes/blockContent.ts
@@ -0,0 +1,48 @@
+import { defineType, defineArrayMember } from 'sanity';
+
+export default defineType({
+  title: 'Block Content',
+  name: 'blockContent',
+  type: 'array',
+  of: [
+    defineArrayMember({
+      title: 'Block',
+      type: 'block',
+      // Styles let you define what blocks can be marked as (e.g. H2, H3, quote)
+      styles: [
+        { title: 'Normal', value: 'normal' },
+        { title: 'H2', value: 'h2' },
+        { title: 'H3', value: 'h3' },
+        { title: 'Quote', value: 'blockquote' },
+      ],
+      lists: [{ title: 'Bullet', value: 'bullet' }],
+      // Marks let you mark up inline text
+      marks: {
+        decorators: [
+          { title: 'Strong', value: 'strong' },
+          { title: 'Emphasis', value: 'em' },
+          { title: 'Code', value: 'code' }
+        ],
+        annotations: [
+          {
+            title: 'URL',
+            name: 'link',
+            type: 'object',
+            fields: [
+              {
+                title: 'URL',
+                name: 'href',
+                type: 'url',
+              },
+            ],
+          },
+        ],
+      },
+    }),
+    // You can add custom image blocks or code snippets here later!
+    defineArrayMember({
+      type: 'image',
+      options: { hotspot: true },
+    }),
+  ],
+});
--- sanity-studio/schemaTypes/index.ts
+++ sanity-studio/schemaTypes/index.ts
@@ -2,5 +2,7 @@ import faq from './faq'
 import project from './project'
 import service from './service'
 import siteSettings from './siteSettings'
+import blockContent from './blockContent';
+import articles from './articles';
 
-export const schemaTypes = [faq, project, service, siteSettings]
+export const schemaTypes = [faq, project, service, siteSettings, blockContent, articles]
